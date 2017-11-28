4fbc44a1c0895307031d9de5368422016d49b0ff
Sortudo <bragafer@usp.br>
2017-11-28 18:45:47 -0200

    Ray Casting

---
+++ Assets/Project/Scripts/PlayerRaycasting.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycasting : MonoBehaviour {
+	public float distanceToSee;
+	RaycastHit whatIHit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
+
+				if(Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee)){
+					Debug.Log("I touched " + whatIHit.collider.gameObject.name);
+					//renderer.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+				}
+	}
+}
