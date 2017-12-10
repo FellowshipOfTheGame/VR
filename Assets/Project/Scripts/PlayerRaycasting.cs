98bbb6f9d9b940d3abb9987e92cae11856a7580c
Sortudo <bragafer@usp.br>
2017-12-10 21:31:08 -0200

    HighLight em objetos

--- Assets/Project/Scripts/PlayerRaycasting.cs
+++ Assets/Project/Scripts/PlayerRaycasting.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 public class PlayerRaycasting : MonoBehaviour {
 	public float distanceToSee;
 	RaycastHit whatIHit;
+	Material m_material;
+	GameObject refe = null;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-				Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
+	void FixedUpdate () {
+		Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
 
-				if(Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee)){
-					Debug.Log("I touched " + whatIHit.collider.gameObject.name);
-					//renderer.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
-					if(whatIHit.collider.gameObject.name == "Sphere" || whatIHit.collider.gameObject.name == "Cube"){
-						Debug.Log("HII");
-					}
-				}
+		bool hit = Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee);
+
+		//Debug.Log("I touched " + whatIHit.collider.gameObject.name);
+		//renderer.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+		if(refe != null){
+			print("entrou");
+			if(refe.tag == "Interagir")
+				refe.GetComponent<Renderer>().material.SetFloat("_Outline", 0);
+		}
+
+		if(hit){
+			refe = whatIHit.collider.gameObject;
+			if(refe.tag == "Interagir"){
+				refe.GetComponent<Renderer>().material.SetFloat("_Outline", 0.1f);
+				if(Input.GetKeyDown("enter"))
+					Destroy(refe);
+			}
+		}
+		else{
+			refe = null;
+		}
+		
 	}
 }
