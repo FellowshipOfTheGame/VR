0963044f5c2b427b53f1f7ea4fa4e011b560ba9e
Yago Pessoa <yagorp15@gmail.com>
2018-01-01 15:56:02 -0200

    Teste casas e cenario na rua

---
+++ Assets/raginbear/01_streetProps/demo/code/misc_toogleshadows.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class misc_toogleshadows : MonoBehaviour {
+
+    public void Toggle()
+    {
+        Light light = transform.GetComponent<Light>();
+        if (light.shadows != LightShadows.None)
+        {
+            light.shadows = LightShadows.None;
+        }
+        else
+        {
+
+            light.shadows = LightShadows.Soft;
+
+        }
+    }
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
