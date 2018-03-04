d072c0ba57f3293c326c65e06906b7fbe7ffeed3
Yago Pessoa <yagorp15@gmail.com>
2018-03-03 21:02:39 -0300

    Luz do poste ajustada e sistema de energia estabelecido

---
+++ Assets/Project/Scripts/EnergyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyController : MonoBehaviour {
+
+    public bool isOn;
+
+    // Use this for initialization
+    void Start () {
+        isOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            PowerSwitch();
+        }
+	}
+
+    void PowerSwitch()
+    {
+        isOn = !isOn;
+    }
+
+    public bool IsOn()
+    {
+        return isOn;
+    }
+}
