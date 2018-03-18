ac53a214595de0c7a7219fab0d8912a59b1973c1
Yago Pessoa <yagorp15@gmail.com>
2018-03-18 19:12:34 -0300

    Mecanica basica do puzzla de ligacao concluida

--- Assets/Project/Scripts/BotoesLigacao.cs
+++ Assets/Project/Scripts/BotoesLigacao.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BotoesLigacao : MonoBehaviour {
+    
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 
-    public void Press ()
+    public void Press()
     {
-        print("Botão");
+        // Debug.Log("Parent: " + transform.parent.parent.name);
+
+        transform.parent.parent.GetComponent<LigacaoController>().Press(name);
     }
 }
