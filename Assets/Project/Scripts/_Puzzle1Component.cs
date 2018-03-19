d2ec944fdaf7b2ae0088f339b98e50942559c0f2
Oliver <oliverbecker@usp.br>
2018-03-19 01:07:20 +0000

    Puzzle1 em produção

---
+++ Assets/Project/Scripts/_Puzzle1Component.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Puzzle1Component : MonoBehaviour {
+
+	private int _Rotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			_rotate_left ();
+		} else if (Input.GetKeyDown (KeyCode.E)) {
+			_rotate_right ();
+		}
+	}
+
+	public void _rotate_left () {
+		_set_rotation (_Rotation + 1);
+	}
+
+	public void _rotate_right() {
+		_set_rotation (_Rotation - 1);
+	}
+
+	public void _set_rotation (int i) {
+		_Rotation = i % 8;
+		transform.eulerAngles = new Vector3 (0, 0, _Rotation * 45);
+	}
+
+	public int _get_rotation () {
+		return _Rotation;
+	}
+}
---
+++ Assets/Project/Scripts/_Puzzle1Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Puzzle1Controller : MonoBehaviour {
+
+	public bool _is_finished;
+	public _Puzzle1Component[] _mecanismo;
+
+	// Use this for initialization
+	void Start () {
+		_is_finished = false;
+		for (int i = 0; i < _mecanismo.Length; ++i) {
+			_mecanismo [i]._set_rotation(Random.Range (1, 7));
+//			_mecanismo [i]._set_rotation(1);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_check_if_finished ();
+	}
+
+	private void _check_if_finished() {
+		int i;
+		for (i = 0; i < _mecanismo.Length; ++i) {
+			if (_mecanismo [i]._get_rotation() != 0) {
+				break;
+			}
+		}
+		if (i >= _mecanismo.Length) {
+			_is_finished = true;
+		}
+	}
+		
+}
