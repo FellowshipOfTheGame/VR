669a547fd79eb9263f47b76e164a53b3af3dc2eb
Yago Pessoa <yagorp15@gmail.com>
2018-06-16 19:39:18 -0300

    Adicionado chave e scripts para cadeado e chave

---
+++ Assets/Project/Scripts/Chave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chave : MonoBehaviour {
+
+	private bool playerHas;
+
+	// Use this for initialization
+	void Start () {
+		playerHas = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetButtonDown("Interact1")){
+			GetKey();
+		}
+	}
+
+	private void GetKey(){
+		playerHas = true;
+		transform.localScale = new Vector3(0, 0, 0);
+	}
+
+	public bool PlayerHas(){
+		return playerHas;
+	}
+}
