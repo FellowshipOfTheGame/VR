9dbb4058004fe75d7e25355e96bf1ce39bbe58f7
Oliver <oliverbecker@usp.br>
2018-07-10 03:27:41 +0000

    Removida a necessidade de apertar E para o Puzzle1
    A parte que fazia necessário primeiro "ativar" o
    puzzle1 (apertando a tecla E quando estivesse pró-
    ximo e olhando para o puzzle) foi removida, para
    facilitar a jogabilidade.

--- Assets/Project/Scripts/_Puzzle1Controller.cs
+++ Assets/Project/Scripts/_Puzzle1Controller.cs
@@ -9,7 +9,7 @@ public class _Puzzle1Controller : MonoBehaviour {
 
 	public _Puzzle1Component[] _mechanism;
 	public bool _isFinished;	// Variável para mostrar se o player já completou ou não o puzzle
-	public bool _isPuzzleActive;
+//	public bool _isPuzzleActive;
 	public float _maxDistance;
 	public PostesController _luzPostes;
 
@@ -20,24 +20,24 @@ public class _Puzzle1Controller : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
 		_isFinished = false;
-		_isPuzzleActive = false;
+//		_isPuzzleActive = false;
 		_mechanism = GetComponentsInChildren<_Puzzle1Component> ();
 		_camera = GameObject.FindGameObjectWithTag ("MainCamera").transform;
 		_player = GameObject.FindGameObjectWithTag ("Player").GetComponent<FirstPersonController> ();
 
 		for (int i = 0; i < _mechanism.Length; ++i) {
 			_mechanism [i]._SetRotation (Random.Range (1, 7));
-			_mechanism [i].GetComponent<EventTrigger> ().enabled = false;
+//			_mechanism [i].GetComponent<EventTrigger> ().enabled = false;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.E) && Vector3.Distance(_camera.position, transform.position) < _maxDistance) {
+/*		if (Input.GetKeyDown(KeyCode.E) && Vector3.Distance(_camera.position, transform.position) < _maxDistance) {
 			GetComponent<EventTrigger> ().enabled = _isPuzzleActive;
 			_ChangePuzzleStatus ();
 		}
-		_CheckIfFinished ();
+*/		_CheckIfFinished ();
 	}
 
 	public void _CheckIfFinished() {		// Confere se todos os mecanismos estão com rotação 0 (posição correta)
@@ -53,22 +53,22 @@ public class _Puzzle1Controller : MonoBehaviour {
 		_isFinished = true;
 		this.enabled = false;		// Desabilita este script e o event trigger quando completa o puzzle
 		transform.GetComponent<EventTrigger> ().enabled = false;
-		_ChangePuzzleStatus ();
+		_DeactivatePuzzle ();
 	}
 
-	public void _ChangePuzzleStatus() {					// Ativa e desativa o puzzle
-		_player.m_MovementEnabled = _isPuzzleActive;				// Controla se o player pode se mover
-		_isPuzzleActive = !_isPuzzleActive;
+	public void _DeactivatePuzzle() {					// Ativa e desativa o puzzle
+//		_player.m_MovementEnabled = _isPuzzleActive;				// Controla se o player pode se mover
+//		_isPuzzleActive = !_isPuzzleActive;
 		for (int i = 0; i < _mechanism.Length; ++i) {		// abilita/desabilita o evento de passar o mouse por cima dos mecanismos
-			_mechanism [i].GetComponent<EventTrigger> ().enabled = _isPuzzleActive;
+			_mechanism [i].GetComponent<EventTrigger> ().enabled = false;
 		}
 
 		int layerMask = 1 << 12;	// Layer do Puzzle1
 		Physics.Raycast (_camera.position, _camera.forward, out _hit, 10, layerMask);
 
 		if (_hit.transform == null)		// Caso não colida com nada, o player estava olhando para fora do puzzle
-			this.enabled = _isPuzzleActive;	// então desativa o puzzle manualmente...
+			this.enabled = false;	// então desativa o puzzle manualmente...
 		else if (_hit.transform.GetComponent<_Puzzle1Component> () != null)	// Caso esteja olhando para um mecanismo
-			_hit.transform.GetComponent<_Puzzle1Component> ().enabled = _isPuzzleActive;	// ativa/desativa ele manualmente
+			_hit.transform.GetComponent<_Puzzle1Component> ().enabled = false;	// ativa/desativa ele manualmente
 	}
 }
