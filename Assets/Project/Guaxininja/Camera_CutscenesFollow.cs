0ff0e0e25d4245400342d7478b5ab19cc4638adf
guaxininja09 <ginja_gabriel@hotmail.com>
2018-07-21 11:55:08 -0300

    controlador de custcene

---
+++ Assets/Project/Guaxininja/Camera_CutscenesFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_CutscenesFollow : MonoBehaviour {
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position;
+	}
+}
--- Assets/Project/Guaxininja/TimelinePlayer.cs
+++ Assets/Project/Guaxininja/TimelinePlayer.cs
@@ -7,19 +7,60 @@ public class TimelinePlayer : MonoBehaviour {
     
     
     public PlayableDirector pd;
-	// Use this for initialization
-	void Start () {
-		
+    public PlayState currstate;
+
+    //player
+    public GameObject Player;
+    public bool lockplayer;
+
+    //camera
+    public GameObject Camera;
+    void Start () {
+        currstate = pd.state;
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
-		
+        CheckState();
+        LockPlayer();
 	}
-    private void OnTriggerEnter(Collider other)
-    {   
-        pd.Play();
-        Debug.Log("cabou");
-
+    private void CheckState()
+    {
+        if (pd.state==PlayState.Playing && currstate!=pd.state)
+        {
+            Debug.Log("rodando");
+            currstate = pd.state;
+        }
+        else if (pd.state == PlayState.Paused && currstate!=pd.state)
+        {
+            Debug.Log("pausado");
+            currstate = pd.state;
+        }
     }
+    private void LockPlayer()
+    {
+        if (lockplayer == true)
+        {
+            if (currstate == PlayState.Playing)
+                
+            {
+                Camera.SetActive(true);
+                Player.SetActive(false);
+            }
+            else if (currstate == PlayState.Paused)
+            {
+                Player.SetActive(true);
+                Camera.SetActive(false);
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            pd.Play();
+        }        
+    }
+
+   
 }
