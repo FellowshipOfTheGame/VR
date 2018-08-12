0f807af30904b21195356da5a54f4194ed753dbd
Oliver <oliverbecker@usp.br>
2018-08-12 01:31:24 +0000

    Abertura do portal final!! :D
    O portão final agora é interagível quando todos os
    cadeados são abertos. Ao interagir, a animação
    (cutscene) do portão começa e o portão se abre.

--- Assets/Project/Scripts/Cadeado.cs
+++ Assets/Project/Scripts/Cadeado.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Cadeado : MonoBehaviour {
 
 	public GameObject chave;
-	private bool abriu;
+	public bool abriu;
+
+	private Renderer[] _renderer;
+	private bool _destruido;
 
 	// Use this for initialization
 	void Start () {
 		abriu = false;
+		_renderer = GetComponentsInChildren<Renderer> ();
+		if (_renderer == null)
+			Debug.Log ("Materiais não encontrados!!!!!!!");
 	}
 	
 	// Update is called once per frame
@@ -21,12 +28,32 @@ public class Cadeado : MonoBehaviour {
 				Open();
 			}
 		}
+		if (abriu && !_destruido) {
+//			Debug.Log ("Leng = " + _renderer.Length);
+			for (int i = 0; i < _renderer.Length; ++i) {
+				Color _color = _renderer [i].material.color;
+				_color.a -= 0.08f;
+				Debug.Log ("alpha[" + i + "] = " + _color.a);
+				if (_color.a >= 0f)
+					_renderer [i].material.color = _color;
+				else
+					_DestroiCadeado ();
+			}
+		}
+	}
+
+	private void _DestroiCadeado () {
+		transform.localScale = new Vector3 (0, 0, 0);
+		_destruido = true;
+		this.enabled = false;
 	}
 
 	private void Open(){
 		if (!abriu){
 			transform.GetChild(1).gameObject.transform.Rotate(0, -75f, 0, Space.Self);
 			abriu = true;
+			GetComponent<EventTrigger> ().enabled = false;
+			GetComponent<Rigidbody> ().useGravity = false;
 		}
 	}
 }
---
+++ Assets/Project/Scripts/_GateController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class _GateController : MonoBehaviour {
+
+	public Cadeado[] _lock;
+	public CutscenePlayer _cutscene;
+
+	private EventTrigger _eventTrigger;
+
+	// Use this for initialization
+	void Start () {
+		if (_lock == null) {
+			Debug.Log ("ERRO! Cadeados não encontrados!!!!!!!!!!");
+			Destroy (this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_CheckIfFinished ();
+	}
+
+	private void _CheckIfFinished() {
+		Debug.Log ("Checando se foi finalizado");
+		for (int i = 0; i < _lock.Length; ++i) {
+			Debug.Log ("cadeado " + i);
+			if (_lock [i].abriu != true)	// Caso o cadeado não esteja aberto, sai da função.
+				return;
+		}
+
+		_eventTrigger = _EventsTrigger.AddComponent (gameObject);
+		_EventsTrigger.AddNewEvent (_eventTrigger, EventTriggerType.PointerClick, (BaseEventData) => {
+			this._OpenGate ();
+		});
+	}
+
+	public void _OpenGate() {
+		Debug.Log ("FIM!!");
+		_cutscene.currscene = 4;
+		_cutscene.startplaying = true;
+	}
+
+}
